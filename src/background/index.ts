98979c960f563f9a10e5dc0970cec7941129ce4e
孤鸿渺影 <iyanghong@foxmail.com>
2023-12-12 17:52:54 +0800

    修复background偶发中断连接问题

--- src/background/index.ts
+++ src/background/index.ts
@@ -24,31 +24,31 @@ const EventHandler: {
 };
 
 /**
+ * Listener for messages from the chrome extension
+ */
+chrome.runtime.onMessage.addListener(function(request: MessageRequestEntity, sender, sendResponse) {
+    /**
+     * If the request has a target and a handler, execute the handler with the request data
+     */
+    if (request.target && EventHandler[request.target] && EventHandler[request.target][request.handler]) {
+        (async () => {
+            try {
+                console.log(`执行事件，source=${request.source}，target=${request.target}，handler=${request.handler}`, request.data);
+                const response = await EventHandler[request.target][request.handler](request.data);
+                sendResponse(response);
+            }catch (e) {
+                console.error(e)
+            }
+        })();
+    }
+    return true;
+});
+
+/**
  * Listener for when the Chrome extension is installed
  */
 chrome.runtime.onInstalled.addListener(async () => {
     console.log('background installed');
-
-    /**
-     * Listener for messages from the chrome extension
-     */
-    chrome.runtime.onMessage.addListener(function(request: MessageRequestEntity, sender, sendResponse) {
-        /**
-         * If the request has a target and a handler, execute the handler with the request data
-         */
-        if (request.target && EventHandler[request.target] && EventHandler[request.target][request.handler]) {
-            (async () => {
-                try {
-                    console.log(`执行事件，source=${request.source}，target=${request.target}，handler=${request.handler}`, request.data);
-                    const response = await EventHandler[request.target][request.handler](request.data);
-                    sendResponse(response);
-                }catch (e) {
-                    console.error(e)
-                }
-            })();
-        }
-        return true;
-    });
 
     /**
      * Get all mock menus and add them to the chrome context menu
